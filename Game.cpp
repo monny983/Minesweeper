6fd3d6ca997afe766d5661d0f67ee39d16271b28
monny983 <74101127+monny983@users.noreply.github.com>
2023-01-18 20:47:35 +0200

    Create Game.cpp
    Add int main()

---
+++ Game.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include<cstring>
+using namespace std;
+
+int main()
+{
+    char playerBoard[max_X_coordinate][max_Y_coordinate] = {};
+
+    char helpingBoard[max_X_coordinate][max_Y_coordinate] = {};
+
+    int matrix_dimension;
+    int enteredMineCount;
+    string command;  // TODO
+    int minesCount;
+    bool playing = true;
+    int x_coordinate, y_coordinate;
+
+    cout << "Hey there, enter a valid dimension! ";
+    cin >> matrix_dimension;
+}
